dae76d526cc30d8d8504e015271a981b0e833850
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2022-11-07 12:43:27 +1100

    fix: css module config

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change Log
+
+## 1.0.1
+
+Fix CSS modules not configured issue.
+
+## 1.0.0
+
+Initial Release
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,3 +1,8 @@
+import fsPromises from 'node:fs/promises';
+import path from 'node:path';
+
+import postCss from 'postcss';
+import postCssModules from 'postcss-modules';
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
@@ -8,4 +13,65 @@ export default defineConfig({
 	env: {
 		NODE_ENV: 'production',
 	},
+	esbuildPlugins: [
+		{
+			name: 'css-module',
+			setup(build): void {
+				build.onResolve(
+					{ filter: /\.module\.css$/, namespace: 'file' },
+					(args) => ({
+						path: `${args.path}#css-module`,
+						namespace: 'css-module',
+						pluginData: {
+							pathDir: path.join(args.resolveDir, args.path),
+						},
+					})
+				);
+				build.onLoad(
+					{ filter: /#css-module$/, namespace: 'css-module' },
+					async (args) => {
+						const { pluginData } = args as {
+							pluginData: { pathDir: string };
+						};
+
+						const source = await fsPromises.readFile(
+							pluginData.pathDir,
+							'utf8'
+						);
+
+						let cssModule = {};
+						const result = await postCss([
+							postCssModules({
+								getJSON(_, json) {
+									cssModule = json;
+								},
+							}),
+						]).process(source, { from: pluginData.pathDir });
+
+						return {
+							pluginData: { css: result.css },
+							contents: `import "${
+								pluginData.pathDir
+							}"; export default ${JSON.stringify(cssModule)}`,
+						};
+					}
+				);
+				build.onResolve(
+					{ filter: /\.module\.css$/, namespace: 'css-module' },
+					(args) => ({
+						path: path.join(args.resolveDir, args.path, '#css-module-data'),
+						namespace: 'css-module',
+						pluginData: args.pluginData as { css: string },
+					})
+				);
+				build.onLoad(
+					{ filter: /#css-module-data$/, namespace: 'css-module' },
+					(args) => ({
+						contents: (args.pluginData as { css: string }).css,
+						loader: 'css',
+					})
+				);
+			},
+		},
+	],
 });
